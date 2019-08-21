27333ac3092ba554f701bbf148e54faa635df9dd
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-08-20 17:52:34 -0700

    encryption working, working on updating usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_many :accounts
     has_many :logins, :through => :accounts
     has_secure_password
-    validates :username, uniqueness: { case_sensitive: false }
-
+    validates :username, uniqueness: { case_sensitive: true }, :length => { :in => 6..20 }
+    validates :password, length: { :in => 8..20 }
 end
